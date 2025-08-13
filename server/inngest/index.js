38008aefc73d10e6f8abd582bc42e47d2abee2a4
Sulyman <sulyman560@gmail.com>
2025-08-13 21:32:04 +0600

    innset error fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import User from "../models/user.js";
+import User from "../models/Users.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
