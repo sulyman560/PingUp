e7d9d716225febef7b446750c7b1308313176297
Sulyman <sulyman560@gmail.com>
2025-08-18 00:09:01 +0600

    email reminder added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,36 @@ import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
 import Login from './pages/Login'
-import { useUser } from '@clerk/clerk-react'
+import { useUser, useAuth } from '@clerk/clerk-react'
 import Layout from './pages/Layout'
 import {Toaster} from 'react-hot-toast'
+import { useEffect } from 'react'
+import axios from 'axios';
 
 const App = () => {
-  const { user } = useUser()
+    const { user } = useUser();
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (!user) return;
+
+      try {
+        // Get JWT from Clerk
+        getToken({ template: 'default' }).then((token) => console.log(token));
+
+        // Call backend with JWT in Authorization header
+        const res = await axios.get('http://localhost:4000/api/user/data', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        console.log(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchUserData();
+  }, [user]);
   return (
     <>
     <Toaster />
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/Users.js";
+import sendMail from "../configs/nodeMailer.js";
+import Connection from "../models/Connections.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -61,9 +63,65 @@ const syncUserDeletion = inngest.createFunction(
     }
 )
 
+// Inngest Function to send reminder when a new connection  request is added
+const sendNewConnectionRequestReminder = inngest.createFunction(
+  {id: "send-new-connection-request-reminder"},
+  {event: "app/connection-request"},
+  async ({event, step}) => {
+    const {connectionId} = event.data;
+
+    await step.run('send-connection-request-null', async () => {
+      const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id')
+      const subject = `New Connection Request`;
+      const body = `
+      <div style="font-family:Arial, sans-serif; padding: 20px;">
+      <h2>Hi ${connection.to_user_id.full_name},</h2>
+      <p>You have a new connection  request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+      <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981;">here</a>to accept or reject the request</p>
+      </br>
+      <p>Thanks,</br>PingUp - stay Connected</p>
+      </div>
+      `
+      await sendMail({
+        to: connection.to_user_id.email,
+        subject,
+        body
+      })
+    })
+
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000)
+    await step.sleepUntil("wait-for-24-hours", in24Hours);
+    await step.run('send-connection-request-reminder', async () => {
+      const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+      if (connection.status === 'accepted') {
+        return {message: "Already accepted"}
+
+      }
+      const subject = `New Connection Request`;
+      const body = `
+      <div style="font-family:Arial, sans-serif; padding: 20px;">
+      <h2>Hi ${connection.to_user_id.full_name},</h2>
+      <p>You have a new connection  request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+      <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981;">here</a>to accept or reject the request</p>
+      </br>
+      <p>Thanks,</br>PingUp - stay Connected</p>
+      </div>
+      `
+      await sendMail({
+        to: connection.to_user_id.email,
+        subject,
+        body
+      })
+      return {message: "Reminder sent."}
+    })
+
+  }
+)
+
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    sendNewConnectionRequestReminder
 ];
